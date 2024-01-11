f3a3dc61a45b3c7901945610f344fed4ff3dc090
flats <hiimhibiscus@gmail.com>
2024-01-11 06:23:34 -0500

    gn 🛏️

--- Common/_ECS/ComponentSystem.cs
+++ Common/_ECS/ComponentSystem.cs
@@ -1,4 +1,17 @@
 ﻿namespace Kiln.Common._ECS {
     public class ComponentSystem {
+        public static int componentTypeCount { get; set; }
+
+        public static bool HasComponent<T>(int entity) where T : Component {
+            if(entity < 0 || entity >= ComponentData<T>.Components.Length)
+                return false;
+            return ComponentData<T>.Components[entity] != null;
+        }
+
+        public static void RemoveComponent<T>(int entity) where T : Component {
+            if (entity < 0 || entity >= ComponentData<T>.Components.Length)
+                return;
+            ComponentData<T>.Components[entity] = null;
+        }
     }
 }
--- Common/_ECS/EntitySystem.cs
+++ Common/_ECS/EntitySystem.cs
@@ -1,9 +1,19 @@
-﻿namespace Kiln.Common._ECS {
+﻿using System.Collections.Concurrent;
+
+namespace Kiln.Common._ECS {
     public class EntitySystem {
         public static List<EntityData> date = new();
         public static List<int> activeEntityIds = new();
         public static List<int> inactiveEntityIds = new();
-        public static Lazy<int> freeEntityIds = new();
+        public static ConcurrentBag<int> freeEntityIds = new();
         public static int nextEntityID;
+
+        public static Entity CreateEntity(bool activated) {
+            int entityId;
+            if (!freeEntityIds.TryTake(out entityId))
+                entityId = nextEntityID++;
+            return new Entity(entityId);
+        }
+
     }
 }
