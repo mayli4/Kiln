1216dc1709a1869d2073a7686133506051faa93f
flats <hiimhibiscus@gmail.com>
2024-02-06 00:12:28 -0500

    add FNA bootstrapper

---
+++ Common/_Assemblies/FNA.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+#if WINDOWS
+namespace Kiln.Common {
+    public static class Bootstrapper {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetDllDirectory(string lpPathName);
+        public static void StartFNA() { 
+            if(Environment.OSVersion.Platform == PlatformID.Win32NT) {
+                SetDllDirectory(Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory,
+                    Environment.Is64BitProcess ? "x64" : "x86"
+                    ));
+            }
+            Environment.SetEnvironmentVariable("FNA_GRAPHICS_ENABLE_HIGHDPI", "1");
+        }    
+    }
+}
+#endif
